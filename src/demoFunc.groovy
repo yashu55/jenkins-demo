e77cbc3df656e27ad4ae9ab6a723d1457fae8e7b
yashu55 <ysp551996@gmail.com>
2021-06-21 11:12:10 +0530

    2 commit

---
+++ src/demoFunc.groovy
@@ -0,0 +1,27 @@
+
+def call(){
+
+pipeline {
+    agent any
+
+    stages {
+        stage('abc') {
+            steps {
+                echo 'Hello dgdfgdfg'
+            }
+        }
+        stage('def') {
+            steps {
+                echo 'Hellodfgdg'
+            }
+        }
+        stage('hij') {
+            steps {
+                echo 'Hello'
+            }
+        }
+    }
+}
+
+
+}
